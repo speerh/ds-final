c0118ce56a0b3287518dc7d5a6d13c84f55fa538
removetooth <89750544+removetooth@users.noreply.github.com>
2022-12-06 14:49:04 -0600

    cleanup done & no duplicate Person records

--- imdb_rip.py
+++ imdb_rip.py
@@ -14,64 +14,84 @@ topShows = ia.get_top250_tv()
 print("Done!")
 
 def formatSanitary(text, *args, **kwargs):
-    return text.replace("'", "&sq;").format(*args, **kwargs).replace("'","''").replace("&sq;","'")
+    return text.replace("'", "&sq;").format(*args, **kwargs).replace("'","''").replace("&sq;","'").replace('&', 'and')
+
+people = {}
+involvementRecords = []
+
+# Person records must be unique.
+# Using this function adds a Wrote/Directed/Acted record
+# and a Person if they don't exist;
+# otherwise, it updates their role flags accordingly.
+# Person, Wrote, Directed, and Acted records are written
+# at the end of the file.
+def addInvRecord(name, person, role, titleID):
+    flags = 0
+    if role == 'wrote':
+        flags = 0b100
+        involvementRecords.append(
+            formatSanitary(wroteInsert, name = name, titleID = titleID)
+            )
+    elif role == 'directed':
+        flags = 0b010
+        involvementRecords.append(
+            formatSanitary(directedInsert, name = name, titleID = titleID)
+            )
+    elif role == 'acted':
+        flags = 0b001
+        involvementRecords.append(
+            formatSanitary(actedInsert, name = name, titleID = titleID)
+            )
+        
+    if not name in people:
+        people[name] = {
+            'contact': name.replace(" ", "") + "@gmail.com",
+            'DOB': person.get("birth date", "0001-01-01"),
+            'roleFlags':flags
+            }
+    else:
+        people[name]['roleFlags'] = people[name]['roleFlags'] | flags
 
 #region SQL templates
 
 mediaInsert = """
-INSERT
-INTO MEDIA (TitleID, Name, Rating, Budget, Synopsis, Country)
+INSERT INTO MEDIA (TitleID, Name, Rating, Budget, Synopsis, Country)
 VALUES ({titleID}, '{name}', '{rating}', {budget}, '{synopsis}', '{country}');
-
 """
 
 genreInsert = """
-INSERT
-INTO Genre(Genre_MediaID, GenreName)
+INSERT INTO Genre(Genre_MediaID, GenreName)
 VALUES({titleID}, '{genre}');
-
 """
 
 langInsert = """
-INSERT
-INTO Languages(Language_MediaID, LanguageName)
+INSERT INTO Languages(Language_MediaID, LanguageName)
 VALUES({titleID}, '{lang}');
-
 """
 
 movieInsert = """
-INSERT
-INTO Movie(MovieID, Runtime)
+INSERT INTO Movie(MovieID, Runtime)
 VALUES({titleID}, {runtime});
-
 """
 
 personInsert = """
-INSERT
-INTO Person(PersonName, Contact, DOB, RoleFlags)
+INSERT INTO Person(PersonName, Contact, DOB, RoleFlags)
 VALUES('{name}', '{contact}', DATE '{dob}', {roleFlags});
-
 """
 
 wroteInsert = """
-INSERT
-INTO Wrote(Writer_Name, Wrote_MediaID)
+INSERT INTO Wrote(Writer_Name, Wrote_MediaID)
 VALUES('{name}', {titleID});
-
 """
 
 directedInsert = """
-INSERT
-INTO Directed(Director_Name, Directed_MediaID)
+INSERT INTO Directed(Director_Name, Directed_MediaID)
 VALUES('{name}', {titleID});
-
 """
 
 actedInsert = """
-INSERT
-INTO Acted(Actor_Name, Acted_MediaID)
+INSERT INTO Acted(Actor_Name, Acted_MediaID)
 VALUES('{name}', {titleID});
-
 """
 
 #endregion
@@ -80,17 +100,18 @@ VALUES('{name}', {titleID});
 # because we don't need two giant loops for this!
 for mov in topMovies[:5] + bottomMovies[:5]:
     #Precisely selects the movie, writer, director, and cast
+    print(f"Creating queries for {mov['title']}")
+    print("Requesting fine data for Movie")
     movie = ia.get_movie(mov.movieID)
+    print("Requesting fine data for Writer")
     writer = ia.get_person(movie['writer'][0].personID)
+    print("Requesting fine data for Director")
     director = ia.get_person(movie['director'][0].personID)
     cast = (movie['cast'])
-
+    
 #create null variables if data isn't found
     budget_nosan = movie.get('box office', {}).get('Budget', 'NULL')
-    budget = ''.join(c for c in budget_nosan if c.isnumeric())
-    
-    #create a plot variable to remove ' from the plot variable
-    movPlot = str(movie['plot'][0]).replace("'", "''")
+    budget = ''.join(c for c in budget_nosan if c.isnumeric()) if budget_nosan != "NULL" else "NULL"
 
     #Create the SQL statement to insert into media
     out = formatSanitary(
@@ -131,157 +152,111 @@ for mov in topMovies[:5] + bottomMovies[:5]:
         )
     file.write(out)
 
-#Insert the crew into people
+    #Insert the crew into people
     #Insert Writer into person
     wname = str(movie['writer'][0])
-    contact = wname.replace(" ", "") + "@gmail.com"
-    writerDOB = writer.get("birth date", "0001-01-01")
-    out = formatSanitary(
-        personInsert,
-        name = wname,
-        contact = contact,
-        dob = writerDOB,
-        roleFlags = 0b100 # TEMPORARY!! TODO: track Person and do bitwise AND on these!
-        )
-    file.write(out)
-
-    #Insert Writer into Wrote
-    out = formatSanitary(
-        wroteInsert,
-        name = wname,
-        titleID = movie.movieID
-        )
-    file.write(out)    
+    addInvRecord(wname, writer, 'wrote', movie.movieID)   
 
     #Insert Director into Person
     dname = str(movie['director'][0])
-    contact = dname.replace(" ", "") + "@gmail.com"
-    directorDOB = director.get("birth date", "0001-01-01")    
-    out = formatSanitary(
-        personInsert,
-        name = dname,
-        contact = contact,
-        dob = directorDOB,
-        roleFlags = 0b010 # TEMPORARY!! TODO: track Person and do bitwise AND on these!
-        )
-    file.write(out)
-
-    #Insert Director into Directed
-    out = formatSanitary(
-        directedInsert,
-        name = dname,
-        titleID = movie.movieID
-        )
-    file.write(out)
+    addInvRecord(dname, director, 'directed', movie.movieID)
 
     #Insert top cast members
     for i in range(2):
+        print(f"Requesting fine data for actor #{i}")
         actor = ia.get_person(cast[i].personID)
         aname = str(cast[i])
-
-        #Insert Actor into Person
-        contact = aname.replace(" ", "") + "@gmail.com"
-        actorDOB = actor.get("birth date", "0001-01-01")
-        out = formatSanitary(
-            personInsert,
-            name = aname,
-            contact = contact,
-            dob = actorDOB,
-            roleFlags = 0b001 # TEMPORARY!! TODO: track Person and do bitwise AND on these!
-            )
-        file.write(out)
-
-        #Insert Actor into Acted
-        out = formatSanitary(
-            actedInsert,
-            name = aname,
-            titleID = movie.movieID
-            )
-        file.write(out)
-print("Movies and Crew Written (Top and Bottom)\n")
+        addInvRecord(aname, actor, 'acted', movie.movieID)
+        
+print("Movies written (Top and Bottom)\n")
 
 #Create the SQL statements for the topShows and Crew
 for sho in range(5):
     #Precisely selects the movie from the list
+    print(f"Creating queries for {topShows[sho]['title']}")
+    print("Requesting fine data for Show")
     show = ia.get_movie(topShows[sho].movieID)
-
+    
     ia.update(show, 'episodes')
     episode = ia.get_movie(show['episodes'][1][1].movieID)
     
     #create null variables if data isn't found
-    try:
-        budget = ''.join(c for c in show['box office']['Budget'] if c.isnumeric())
-    except KeyError:
-        budget = 'NULL'
-
-    #create a plot variable to remove ' from the plot variable
-    showPlot = str(show['plot'][0]).replace("'", "''")
+    budget_nosan = show.get('box office', {}).get('Budget', 'NULL')
+    budget = ''.join(c for c in budget_nosan if c.isnumeric()) if budget_nosan != "NULL" else "NULL"
     
     #Create the SQL statement to insert into table
-    out = "INSERT\nINTO Media(TitleID, Name, Rating, Budget, Synopsis, Country)\nVALUES(" + show.movieID + ", \'" + show['title'] + "\', \'" + str(show['rating']) + "\', " + budget + ", \'" + showPlot + "\', \'" + str(show["countries"][0]) + "\')\n\n"
+    out = formatSanitary(
+        mediaInsert,
+        titleID = show.movieID,
+        name = show['title'],
+        rating = str(show['rating']),
+        budget = budget,
+        synopsis = str(show['plot'][0]),
+        country = str(show["countries"][0])   
+    )
     file.write(out)
 
     #Create the SQL statement to insert into genre
-    for genre in movie['genres']:
-        out = "INSERT\nINTO Genre(Genre_MediaID, GenreName)\nVALUES(" + show.movieID + ", \'" + genre + "\');\n\n"
+    for genre in show['genres']:
+        out = formatSanitary(
+            genreInsert,
+            titleID = show.movieID,
+            genre = genre
+            )
         file.write(out)
 
     #create the SQL statement to insert languages
-    for language in movie['languages']:
-        out = "INSERT\nINTO Languages(Language_MediaID, LanguageName)\nVALUES(" + show.movieID + ", \'" + language + "\');\n\n"
+    for language in show['languages']:
+        out = formatSanitary(
+            langInsert,
+            titleID = show.movieID,
+            lang = language
+            )
         file.write(out)
 #Insert the crew into people
     #Insert Writer into person
     
     try:
+        print("Requesting fine data for Writer")
         writer = ia.get_person(show['writer'][0].personID)
-
-        #insert writer into person
-        contact = str(movie['writer'][0]).replace(" ", "") + "@gmail.com"
-        try:
-            writerDOB = writer["birth date"]
-        except KeyError:
-            writerDOB = "0001-01-01"
-        out = "INSERT\nINTO Person(PersonName, Contact, DOB, RoleFlags)\nVALUES(\'" + str(show['writer'][0]) + "\', \'" + contact + "\', DATE \'" + writerDOB + "\', " + str(0b100) + ");\n\n"
-        file.write(out)
-
-        #Insert Writer into Wrote
-        out = "INSERT\nINTO Wrote(Writer_Name, Wrote_MediaID)\nVALUES(\'" + str(show['writer'][0]) + "\', " + show.movieID + ");\n\n"
-        file.write(out)    
+        addInvRecord(str(show['writer'][0]), writer, 'wrote', show.movieID)  
     except KeyError:
         pass
 
     try:
+        print("Requesting fine data for Director")
         director = ia.get_person(show['director'][0].personID)
-
         #Insert Director into Person
-        contact = str(movie['director'][0]).replace(" ", "") + "@gmail.com"        
-        out = "INSERT\nINTO Person(PersonName, Contact, DOB, RoleFlags)\nVALUES(\'" + str(show['director'][0]) + "\', \'" + contact + "\', DATE \'" + director['birth date'] + "\', " + str(0b010) + ");\n\n"
-        file.write(out)
-
-        #Insert Director into Directed
-        out = "INSERT\nINTO Directed(Director_Name, Directed_MediaID)\nVALUES(\'" + str(show['director'][0]) + "\', " + show.movieID + ");\n\n"
-        file.write(out)
+        addInvRecord(str(show['director'][0]), director, 'directed', show.movieID)
+        
     except KeyError:
         pass
 
     #Insert top cast members
+    cast = show.get('cast', [])
     for i in range(2):
         try:
-            cast = (show['cast'])
+            print(f"Requesting fine data for actor #{i}")
             actor = ia.get_person(cast[i].personID)
-
-            #Insert Actor into Person
-            contact = str(cast[i]).replace(" ", "") + "@gmail.com"
-            out = "INSERT\nINTO Person(PersonName, Contact, DOB, RoleFlags)\nVALUES(\'" + str(cast[i]) + "\', \'" + contact + "\', DATE \'" + actor['birth date'] + "\', " + str(0b001) + ");\n\n"
-            file.write(out)
-
-            #Insert Actor into Acted
-            out = "INSERT\nINTO Acted(Actor_Name, Acted_MediaID)\nVALUES(\'" + str(cast[i]) + "\', " + show.movieID + ");\n\n"
-            file.write(out)
+            addInvRecord(str(cast[i]), actor, 'acted', show.movieID)
         except KeyError:
             pass
 print("Top Shows Written\n")
+
+print("Writing crew records.....")
+for pname in people:
+    p = people[pname]
+    out = formatSanitary(
+        personInsert,
+        name = pname,
+        contact = p['contact'],
+        dob = p['DOB'],
+        roleFlags = p['roleFlags']
+        )
+    file.write(out)
+
+[file.write(i) for i in involvementRecords]
+print("Done!")
 
 #Close the file when complete with the statements
 file.close()

15dd303ba403c37033350c7352a7ea9b28a80a09
removetooth <89750544+removetooth@users.noreply.github.com>
2022-12-06 09:50:41 -0600

    sql changes
    - ratings are now 4 characters max, default N/A
    - Videos and Photos no longer exist
    - all ids are Number (7) now
    - instead of video and photo data, use links (max 200 characters)

--- imdb2.sql
+++ imdb2.sql
@@ -13,8 +13,6 @@ DROP TABLE Person CASCADE CONSTRAINTS;
 DROP TABLE Wrote CASCADE CONSTRAINTS;
 DROP TABLE Directed CASCADE CONSTRAINTS;
 DROP TABLE Acted CASCADE CONSTRAINTS;
-DROP TABLE Videos CASCADE CONSTRAINTS;
-DROP TABLE Photos CASCADE CONSTRAINTS;
 DROP TABLE MediaVideo CASCADE CONSTRAINTS;
 DROP TABLE MediaPhoto CASCADE CONSTRAINTS;
 DROP TABLE ActingSample CASCADE CONSTRAINTS;
@@ -24,15 +22,15 @@ DROP TABLE WritingSample CASCADE CONSTRAINTS;
 CREATE TABLE Media (
 TitleID		NUMBER (7) PRIMARY KEY,
 Name		VARCHAR2 (50) NOT NULL,
-Rating		VARCHAR2 (10) DEFAULT 'Not rated',
+Rating		VARCHAR2 (4) DEFAULT 'N/A',
 Budget		NUMBER,
 Synopsis	VARCHAR2 (500) DEFAULT 'No description given',
 Country		VARCHAR2 (30)
 );
 
 CREATE TABLE More_Like (
-ReferenceID	NUMBER (16),
-SimilarID	NUMBER (16),
+ReferenceID	NUMBER (7),
+SimilarID	NUMBER (7),
 CONSTRAINT More_Like_pk PRIMARY KEY (ReferenceID, SimilarID),
 CONSTRAINT More_Like_ReferenceID_fk FOREIGN KEY (ReferenceID) REFERENCES Media(TitleID),
 CONSTRAINT More_Like_SimilarID_fk FOREIGN KEY (SimilarID) REFERENCES Media(TitleID)
@@ -53,27 +51,27 @@ CONSTRAINT Language_MediaID_fk FOREIGN KEY (Language_MediaID) REFERENCES Media(T
 );
 
 CREATE TABLE ViewOpt (
-ViewOpt_MediaID	NUMBER(16),
+ViewOpt_MediaID	NUMBER (7),
 ViewOptName	VARCHAR (20),
 CONSTRAINT ViewOpt_pk PRIMARY KEY (ViewOpt_MediaID, ViewOptName),
 CONSTRAINT ViewOpt_MediaID_fk FOREIGN KEY (ViewOpt_MediaID) REFERENCES Media(TitleID)
 );
 
 CREATE TABLE Movie (
-MovieID		NUMBER (16) PRIMARY KEY,
+MovieID		NUMBER (7) PRIMARY KEY,
 Runtime		NUMBER NOT NULL,
 CONSTRAINT Movie_MovieID_fk FOREIGN KEY (MovieID) REFERENCES Media(TitleID)
 );
 
 CREATE TABLE TV_Show (
-TVID		NUMBER (16) PRIMARY KEY,
+TVID		NUMBER (7) PRIMARY KEY,
 Start_Date	DATE NOT NULL,
 End_Date	DATE,
 CONSTRAINT TV_Show_TVID_fk FOREIGN KEY (TVID) REFERENCES Media(TitleID)
 );
 
 CREATE TABLE Season (
-ShowID		NUMBER (16),
+ShowID		NUMBER (7),
 Season_No	NUMBER (3),
 Name		VARCHAR2 (30),
 CONSTRAINT Season_pk PRIMARY KEY (ShowID, Season_No),
@@ -81,10 +79,10 @@ CONSTRAINT Season_ShowID_fk FOREIGN KEY (ShowID) REFERENCES TV_Show(TVID)
 );
 
 CREATE TABLE Episode (
-Ep_ShowID	NUMBER (16),
+Ep_ShowID	NUMBER (7),
 Ep_Season_No	NUMBER (3),
 Episode_No	NUMBER (3),
-Ep_Rating	VARCHAR2 (10) DEFAULT 'Not rated',
+Ep_Rating	VARCHAR2 (4) DEFAULT 'N/A',
 Ep_Air_Date	DATE,
 Ep_Length	NUMBER NOT NULL,
 Ep_Synopsis	VARCHAR2 (200) DEFAULT 'No description given',
@@ -97,14 +95,14 @@ Award_Title	VARCHAR2 (20),
 Organization	VARCHAR2 (40),
 Category	VARCHAR2 (25),
 Year		NUMBER (4),
-Award_TitleID	NUMBER (16) NOT NULL,
+Award_TitleID	NUMBER (7) NOT NULL,
 Status		VARCHAR2 (10),
 CONSTRAINT Award_pk PRIMARY KEY (Award_Title, Organization, Category, Year),
 CONSTRAINT Award_TitleID_fk FOREIGN KEY (Award_TitleID) REFERENCES Media(TitleID)
 );
 
 CREATE TABLE Review (
-Review_MediaID	NUMBER (16),
+Review_MediaID	NUMBER (7),
 Reviewer	VARCHAR2 (30),
 ReviewDate	DATE NOT NULL,
 ReviewBody	VARCHAR2 (4000) NOT NULL,
@@ -126,7 +124,7 @@ RoleFlags	NUMBER (1)
 
 CREATE TABLE Wrote (
 Writer_Name	VARCHAR2 (30),
-Wrote_MediaID	NUMBER (16),
+Wrote_MediaID	NUMBER (7),
 CONSTRAINT Wrote_pk PRIMARY KEY (Writer_Name, Wrote_MediaID),
 CONSTRAINT Wrote_Name_fk FOREIGN KEY (Writer_Name) REFERENCES Person(PersonName),
 CONSTRAINT Wrote_MediaID_fk FOREIGN KEY (Wrote_MediaID) REFERENCES Media(TitleID)
@@ -134,7 +132,7 @@ CONSTRAINT Wrote_MediaID_fk FOREIGN KEY (Wrote_MediaID) REFERENCES Media(TitleID
 
 CREATE TABLE Directed (
 Director_Name	VARCHAR2 (30),
-Directed_MediaID NUMBER (16),
+Directed_MediaID NUMBER (7),
 CONSTRAINT Directed_pk PRIMARY KEY (Director_Name, Directed_MediaID),
 CONSTRAINT Directed_Name_fk FOREIGN KEY (Director_Name) REFERENCES Person(PersonName),
 CONSTRAINT Directed_MediaID_fk FOREIGN KEY (Directed_MediaID) REFERENCES Media(TitleID)
@@ -142,52 +140,38 @@ CONSTRAINT Directed_MediaID_fk FOREIGN KEY (Directed_MediaID) REFERENCES Media(T
 
 CREATE TABLE Acted (
 Actor_Name	VARCHAR2 (30),
-Acted_MediaID	NUMBER (16),
+Acted_MediaID	NUMBER (7),
 CONSTRAINT Acted_pk PRIMARY KEY (Actor_Name, Acted_MediaID),
 CONSTRAINT Acted_Name_fk FOREIGN KEY (Actor_Name) REFERENCES Person(PersonName),
 CONSTRAINT Acted_MediaID_fk FOREIGN KEY (Acted_MediaID) REFERENCES Media(TitleID)
 );
 
-CREATE TABLE Videos (
-VideoID		NUMBER (16) PRIMARY KEY,
-VideoLink	VARCHAR2
-);
-
-CREATE TABLE Photos (
-PhotoID		NUMBER (16) PRIMARY KEY,
-PhotoLink	VARCHAR2
-);
-
 CREATE TABLE MediaVideo (
-MV_MediaID	NUMBER (16),
-MV_VideoID	NUMBER (16),
-CONSTRAINT MediaVideo_pk PRIMARY KEY (MV_MediaID, MV_VideoID),
-CONSTRAINT MediaVideo_MediaID_fk FOREIGN KEY (MV_MediaID) REFERENCES Media(TitleID),
-CONSTRAINT MediaVideo_VideoID_fk FOREIGN KEY (MV_VideoID) REFERENCES Videos(VideoID)
+MV_MediaID	NUMBER (7),
+MV_VideoLink	VARCHAR2 (200),
+CONSTRAINT MediaVideo_pk PRIMARY KEY (MV_MediaID, MV_VideoLink),
+CONSTRAINT MediaVideo_MediaID_fk FOREIGN KEY (MV_MediaID) REFERENCES Media(TitleID)
 );
 
 CREATE TABLE MediaPhoto (
-MP_MediaID	NUMBER (16),
-MP_PhotoID	NUMBER (16),
-CONSTRAINT MediaPhoto_pk PRIMARY KEY (MP_MediaID, MP_PhotoID),
-CONSTRAINT MediaPhoto_MediaID_fk FOREIGN KEY (MP_MediaID) REFERENCES Media(TitleID),
-CONSTRAINT MediaPhoto_PhotoID_fk FOREIGN KEY (MP_PhotoID) REFERENCES Photos(PhotoID)
+MP_MediaID	NUMBER (7),
+MP_PhotoLink	VARCHAR2 (200),
+CONSTRAINT MediaPhoto_pk PRIMARY KEY (MP_MediaID, MP_PhotoLink),
+CONSTRAINT MediaPhoto_MediaID_fk FOREIGN KEY (MP_MediaID) REFERENCES Media(TitleID)
 );
 
 CREATE TABLE ActingSample (
 AV_PersonName	VARCHAR2 (30),
-AV_VideoID	NUMBER (16),
-CONSTRAINT ActingSample_pk PRIMARY KEY (AV_PersonName, AV_VideoID),
-CONSTRAINT ActingSample_PersonName_fk FOREIGN KEY (AV_PersonName) REFERENCES Person(PersonName),
-CONSTRAINT ActingSample_VideoID_fk FOREIGN KEY (AV_VideoID) REFERENCES Videos(VideoID)
+AV_VideoLink	VARCHAR2 (200),
+CONSTRAINT ActingSample_pk PRIMARY KEY (AV_PersonName, AV_VideoLink),
+CONSTRAINT ActingSample_PersonName_fk FOREIGN KEY (AV_PersonName) REFERENCES Person(PersonName)
 );
 
 CREATE TABLE PersonPhoto (
 PP_PersonName	VARCHAR2 (30),
-PP_PhotoID	NUMBER (16),
-CONSTRAINT PersonPhoto_pk PRIMARY KEY (PP_PersonName, PP_PhotoID),
-CONSTRAINT PersonPhoto_PersonName_fk FOREIGN KEY (PP_PersonName) REFERENCES Person(PersonName),
-CONSTRAINT PersonPhot_PhotoID_fk FOREIGN KEY (PP_PhotoID) REFERENCES Photos(PhotoID)
+PP_PhotoLink	VARCHAR2 (200),
+CONSTRAINT PersonPhoto_pk PRIMARY KEY (PP_PersonName, PP_PhotoLink),
+CONSTRAINT PersonPhoto_PersonName_fk FOREIGN KEY (PP_PersonName) REFERENCES Person(PersonName)
 );
 
 CREATE TABLE WritingSample (
